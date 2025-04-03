c403af8dee8c4132e597382fad6d2e51c964dd82
Steve <sthlm_32@hotmail.com>
2025-04-03 17:53:19 +0200

    Added comment

--- app/stalarm.py
+++ app/stalarm.py
@@ -42,8 +42,8 @@ for symbol in symbols:
         company_name = stock_data_dict.get("longName", "N/A")
 
         # Construct the URL to the stock's Yahoo Finance page
-        stock_url = f"https://finance.yahoo.com/quote/{symbol}"
-        print(f"URL to {company_name} ({symbol}) stock: {stock_url}") # //////////////////////////////////////////////////////////
+        stock_url = f"https://finance.yahoo.com/quote/{symbol}" # Not used ////////////////////////////////////////////////////////
+        # print(f"URL to {company_name} ({symbol}) stock: {stock_url}") # //////////////////////////////////////////////////////////
 
         # Get historical data since start date
         historical_data = stock_info.history(start=start_date)
